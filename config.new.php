ad484f8f00d010c0c3e8c8cdf1e818c080c3a1c9
呼吸二氧化碳 <jonwang@queyang.com>
2012-02-23 21:43:28 +0800

    配置项目完善，index.php加入负载保护代码

--- config.new.php
+++ config.new.php
@@ -1,34 +1,132 @@
 <?php
 
 /**
+ * 项目配置
+ *
+ * 系统每一次请求都是根据请求的URL来匹配是哪个项目，如果匹配不到则使用默认的application作为默认项目
+ * 而在projects里的每一个项目的include_path都会包含application和core这两个目录
+ *
+ * 每一个项目都包含name和url和admin_url参数，分别表示项目的名称和前端URL和后台URL（2.0中的isuse参数被废除）
+ * 其中项目的url和admin_url可以是字符串也可以是数组，可以/开头，也可以http://或https://开头。必须/结尾（2.0版本中不需要/结尾,系统在初始化时会进行处理，不存在的则会补上“/”）
+ *
+ * 主意：admin_url若配置成/开头，则它完整URL应该是接着url参数的，会受url参数影响。比如：
+ *
+ *   $config['projects']['test'] = array('name'=>'test','url'=>'http://www.abc.com/test/','admin_url'=>'/admin/');
+ *
+ * 那么实际上后台的URL应该是 http://www.abc.com/test/admin/ ，而不是 http://www.abc.com/admin/ 而若配置成下面这样，用http://或https://开头：
+ *
+ *   test = array('name'=>'test','url'=>'http://www.abc.com/test/','admin_url'=>'http://admin.abc.com/');
+ *
+ * 那么后台实际的URL就是 http://admin.abc.com/ ，不会受到url参数的影响
+ *
+ * @var array
+ */
+$config['projects'] = array
+(
+    'docs' => array
+    (
+        'name' => '项目手册',
+        'url'  => '/docs/',
+    ),
+    'cms' => array
+    (
+        'name' => 'CMS',
+        'url'  => 'http://cms.myqee.com/',
+        'admin_url' => '/admin/',
+    ),
+    //... 可以增加自己的项目
+);
+
+
+/**
+ * 设定的固定的APP的URL，不设置则可使用默APP URL可访问
+ *
+ * 类似项目配置中URL的设定，系统每次都会根据请求的URL去判断是否属于某个APP，其优先级低于项目中URL
+ * url可以是字符串也可以是数组，可以/开头，也可以http://开头。必须/结尾（2.0版本中不需要/结尾,系统在初始化时会进行处理，不存在的则会补上“/”）
+ *
+ * @var array
+ */
+$config['apps_url'] = array
+(
+//    'bigdir/appdir' => 'http://test.app.abc.com/',
+);
+
+
+/**
  * 加载类库配置
  *
  * 将指定加载libraries目录的类库
  *
  * @var array
  */
-$config['core']['libraries'] = array(
+$config['core']['libraries'] = array
+(
     // 系统在初始化时就会自动加载的类库，必须是bigDir/subDir形式，例如: mysite/test 表示加载 libraries/mysite/test/ 目录的类库，下同
-    'autoload' => array(
+    'autoload' => array
+    (
 
     ),
 
     // 命令行下会加载的类库
-    'cli'      => array(
+    'cli'  => array
+    (
 
+    ),
+
+    // APP模式下会加载的类库
+    'app'  => array
+    (
+        'myqee/for_app',
     ),
 
     // 调试环境下会加载的类库
-    'debug'    => array(
+    'debug' => array
+    (
         'myqee/develop',
     ),
 
-    // 后台模式时加载的类库
-    'admin'    => array(
+    // 后台模式下会加载的类库
+    'admin' => array
+    (
         'myqee/administration',
     ),
 );
 
+
+/**
+ * 网站根目录，可以http:// 或 https:// 或 / 开头，必须/结尾
+ *
+ * @var string
+ */
+$config['core']['url']['site'] = '/';
+
+
+/**
+ * 默认网站后台目录，可以http:// 或 https:// 或 / 开头，必须/结尾
+ *
+ * @var string
+ */
+$config['core']['url']['admin'] = '/admin/';
+
+
+/**
+ * 应用默认URL，可以http:// 或 https:// 或 / 开头，必须/结尾
+ *
+ * 如果是独立域名，请配置站点路径到apps目录
+ *
+ * @var string
+ */
+$config['core']['url']['apps'] = '/apps/';
+
+
+/**
+ * 应用程序URL，可以http:// 或 https:// 或 / 开头，必须/结尾
+ *
+ * @var string
+ */
+$config['core']['url']['statics'] = '/statics/';
+
+
 /**
  * 页面编码
  *
@@ -36,47 +134,17 @@ $config['core']['libraries'] = array(
  */
 $config['core']['charset'] = 'utf-8';
 
+
 /**
- * 后台控制器模式URL前缀
- *
- *   array(
- *      '/admin/',                   // 所有/admin开头的URL都将被纳入后台控制器模式
- *      'http://admin.test.com/',    // 所有admin.test.com域名下的请求将被纳入后台控制器模式
- *   )
+ * 开启调试模式的密码，支持多个,key为用户名，value为密码，清空则表示禁用开启debug模式
  *
  * @var array
  */
-$config['core']['admin_mode_base_url'] = array(
-    '/admin/',
+$config['core']['debug_open_password'] = array
+(
+    'myqee' => '123456',
 );
 
-/**
- * 开启在线调试模式的密码，支持多个，清除密码则关闭在线调试功能
- *
- * 在线调试地址为根目录下 /opendebugger
- * 例如 http://www.mysite.com/opendebugger
- *
- * @var array
- */
-$config['core']['debug_open_password'] = array(
-        'myqee',
-);
-
-/**
- * 页面编码
- *
- * @var string
- */
-$config['core']['charset'] = 'utf-8';
-
-/**
- * 网站根目录
- *
- * 设为null则自动判断
- *
- * @var string
- */
-$config['core']['base_url'] = null;
 
 /**
  * 错误等级
@@ -85,29 +153,35 @@ $config['core']['base_url'] = null;
  */
 $config['core']['error_reporting'] = 7;
 
+
 /**
- * 服务器文件夹默认文件
- *
- * 通常为 index.html 或 index.htm 等，只需第一个即可
+ * 服务器默认文件夹文件，取第一个即可
  *
  * @var string
+ * @example index.html
  */
 $config['core']['server_index_page'] = 'index.html';
 
+
 /**
  * 默认时区
+ *
+ * PRC = 中国标准时区
  *
  * @var string
  * @see http://www.php.net/manual/en/timezones.php
  */
 $config['core']['timezone'] = 'PRC';
 
+
 /**
  * 默认语言包
  *
  * @var string
+ * @example zh-cn
  */
 $config['core']['lang'] = 'zh-cn';
+
 
 /**
  * 是否允许在线安装(及删除)应用
@@ -116,41 +190,28 @@ $config['core']['lang'] = 'zh-cn';
  */
 $config['core']['online_install_apps'] = true;
 
-/**
- * WEB服务的服务器列表，留空则禁用同步功能（比如只有1台web服务器时请禁用此功能）
- *
- * 配置服务器后，可以实现服务器上data目录的文件同步功能，同步逻辑通过本系统完成，如果已经配置了data目录的sync同步机制，只需要配置1个主服务器即可
- * 可通过 \Sync::exec($function,$param_1,$param_2,...); 实现在所有服务器上各自运行一遍$function
- *
- *	 //可以是内网IP，确保服务器之间可以相互访问到，端口请确保指定到apache/IIS/nginx等端口上
- *   array(
- * 	   '192.168.1.1',		//80端口可省略:80
- * 	   '192.168.1.2:81',
- * 	   '192.168.1.3:81',
- *   )
- *
- * @var array
- */
-$config['core']['web_server_list'] = array(
-
-);
 
 /**
  * 调试环境打开关键字
  *
+ * 建议本地开发时设置打开调试环境，生产环境不设置php.ini即可
+ *
  * 例如设置为 myqee.debug 则在php.ini中加入：
  *
+ *   [MyQEE]
  *   myqee.debug = On
  *
  * 例如设置为 mytest.abc 则在php.ini中加入：
  *
+ *   [MyQEE]
  *   mytest.abc = On
  *
- * 即可设置为默认打开调试模式，系统会自动加载$config['core']['libraries']['debug']中类库，且处于其它类库之上的优先级，建议本地开发时设置，生产环境不设置php.ini即可
+ * 即可设置为默认打开调试模式，系统会自动加载$config['core']['libraries']['debug']中类库，且处于其它类库之上的优先级
  *
  * @var string
  */
-$config['core']['local_debug_cfg'] = 'myqee.debug';
+$config['core']['local_debug_cfg'] = 'm1yqee.debug';
+
 
 /**
  * 默认数据库配置
@@ -159,20 +220,20 @@ $config['core']['local_debug_cfg'] = 'myqee.debug';
  */
 $config['database']['default'] = array
 (
-    'type'       => 'mysqli',                //表示用mysqli类库连接，系统默认支持 mysql|mysqli|mango ，需要别的可自行扩展
-    'connection' => array(
-            'hostname'   => '127.0.0.1',     //服务器IP或域名
-            'database'   => 'test',          //库名称
-            'username'   => 'root',          //数据库用户
-            'password'   => '123456',        //密码
+    'type'       => 'mysqli',
+//  'connection' => 'mysql://root:123456@127.0.0.1/test/',
+    'connection' => array
+    (
+        'hostname'   => '127.0.0.1',
+        'database'   => 'test',
+        'username'   => 'root',
+        'password'   => '123456',
+        'persistent' => false,
     ),
-    'table_prefix' => '',                    //表前缀，比如:text_
-    'charset'      => 'utf8',                //表编码
-    'caching'      => false,                 //是否使用同一进程SQL查询缓存（可防止同一SQL语句反复查询）
+    'table_prefix' => '',
+    'charset'      => 'utf8',
+    'caching'      => false,
+    'profiling'    => true,
 );
-
-
-
-
 
 
--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,7 +1,69 @@
 <?php
+/**
+ * 服务器负载保护函数，本方法目前不支持window系统
+ *
+ * @see http://php.net/manual/en/function.sys-getloadavg.php
+ */
+$load_protection = function()
+{
+    // 最大负载不要超过3*N核，例如有16核（含8核超线程）则 16*3=48
+    $max_load_avg = 48;
 
-chdir(dirname(__FILE__));
+    if ( !function_exists('sys_getloadavg') )
+    {
+        return false;
+    }
 
-include '../core/bootstrap.php';
+    // 获取load average
+    $load = sys_getloadavg();
 
+    if ( !isset($load[0]) )
+    {
+        return false;
+    }
+
+    if ( $load[0] <= $max_load_avg )
+    {
+        // 未超过负载，则跳出
+        return false;
+    }
+
+    $msg_tpl = "[%s] HOST:%s LOAD:%s ARGV/URI:%s\n";
+    $time = @date(DATE_RFC2822);
+    $host = php_uname('n');
+    $load = sprintf('%.2f', $load[0]);
+    if ( php_sapi_name() == "cli" || empty($_SERVER['PHP_SELF']) )
+    {
+        $argv_or_uri = implode(',', $argv);
+    }
+    else
+    {
+        $argv_or_uri = $_SERVER['REQUEST_URI'];
+    }
+
+    $msg = sprintf($msg_tpl, $time, $host, $load, $argv_or_uri);
+
+    $log_dir = dirname(__FILE__).'/../data/logs/';
+    if ( @is_dir($log_dir) )
+    {
+        // 写入日志
+        @file_put_contents( $log_dir.'php-server-overload.log', $msg, FILE_APPEND );
+    }
+
+    // 输出500错误
+    header("HTTP/1.1 503 Too busy, try again later");
+    header("Expires: " . gmdate("D, d M Y H:i:s", time()-999999) . " GMT");
+    header("Cache-Control: private");
+    header("Pragma: no-cache");
+
+    exit( file_get_contents( dirname(__FILE__).'/errors/server_overload.html') );
+};
+$load_protection();
+unset($load_protection);
+
+
+// 加载系统
+include dirname(__FILE__).'/../core/bootstrap.php';
+
+// 执行bootstrap
 Bootstrap::setup();
